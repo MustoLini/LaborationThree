a5213a672dbb32dce327e2fa3d462eb647b713d1
Johannes Danielsson <danielssonjohannes@gmail.com>
2022-10-18 14:38:40 +0200

    Add onMouseClickEvent

--- src/main/java/se/iths/jd/javafxttlabthree/HelloController.java
+++ src/main/java/se/iths/jd/javafxttlabthree/HelloController.java
@@ -1,9 +1,15 @@
 package se.iths.jd.javafxttlabthree;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.paint.Color;
 
 public class HelloController {
+    @FXML
+    private Button button1;
     @FXML
     private Label welcomeText;
 
@@ -11,4 +17,13 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+    @FXML
+    void labelClicked(MouseEvent mouseEventevent) {
+        if (mouseEventevent.isControlDown()){
+            welcomeText.setBackground(Background.fill(Color.RED));
+        }
+    }
+
+
+
 }

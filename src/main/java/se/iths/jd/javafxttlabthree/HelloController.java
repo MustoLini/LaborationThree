4dd6daa47d88d03e5fb43ec80415b22f8f16d555
MustoLini <danielssonjohannes@gmail.com>
2022-10-26 13:04:44 +0200

    Adding eraser method and making list and making choicebox

--- src/main/java/se/iths/jd/javafxttlabthree/HelloController.java
+++ src/main/java/se/iths/jd/javafxttlabthree/HelloController.java
@@ -4,102 +4,106 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.ColorPicker;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.Slider;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import se.iths.jd.javafxttlabthree.shapes.Circle;
 import se.iths.jd.javafxttlabthree.shapes.Rectangle;
 import se.iths.jd.javafxttlabthree.shapes.Triangle;
 import se.iths.jd.javafxttlabthree.shapes.shapesMainClass.Shapes;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Queue;
 
 
 public class HelloController {
-    Queue<Shapes> shapes = new ArrayDeque<>();
+    List<Shapes> shapes = new ArrayList<>();
     @FXML
     private Slider sizeOfBrush;
     @FXML
     private Canvas canvas;
+    String[]shapeChoices= new String[]{"Draw Normal","Make a Circle", "Make a Rectangle", "Make a Triangle", "Eraser"};
     @FXML
     private ColorPicker colorPicker;
     @FXML
-    private RadioButton rectangleButton;
-    @FXML
-    private RadioButton triangleButton;
-    @FXML
-    private RadioButton circleButton;
-    @FXML
-    private ToggleGroup shapesGroupButton;
-    @FXML
-    private RadioButton eraser;
-    @FXML
-    private RadioButton line;
+    private ChoiceBox<String> whatShapeToPick;
+
+
 
     @FXML
     void closeProgram(ActionEvent event) {
         System.exit(1);
     }
-
+    @FXML
     public void initialize() {
-
-        makeACircle();
-        makeLines();
-
+        whatShapeToPick.getItems().addAll(shapeChoices);
+    }
+    @FXML
+    public void whatShapeYouHavePicked(ActionEvent event){
+        String value = whatShapeToPick.getValue();
+        if ("Draw Normal".equals(value)) {
+            drawNormal();
+        } else if ("Make a Circle".equals(value)) {
+            makeACircle();
+        } else if ("Make a Triangle".equals(value)) {
+            makeATriangle();
+        } else if ("Make a Rectangle".equals(value)) {
+            makeARectangle();
+        }else if ("Eraser".equals(value)){
+            eraser();
+        }
     }
 
-    void makeLines() {
 
-        if (line.isSelected()) {
+    void drawNormal() {
+
+
             GraphicsContext gc = canvas.getGraphicsContext2D();
             canvas.setOnMouseDragged(mouseEvent -> {
                 double size = sizeOfBrush.getValue();
                 double x = mouseEvent.getX() - size / 2;
                 double y = mouseEvent.getY() - size / 2;
-
-                if (eraser.isSelected()) {
-                    gc.clearRect(x, y, size, size);
-                } else {
-                    gc.setFill(colorPicker.getValue());
-                    gc.fillRect(x, y, size, size);
-                }
+                gc.setFill(colorPicker.getValue());
+                gc.fillRect(x, y, size, size);
 
             });
-        }
+
     }
+    void eraser(){
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        canvas.setOnMouseDragged(mouseEvent -> {
+            double size = sizeOfBrush.getValue();
+            double x = mouseEvent.getX() - size / 2;
+            double y = mouseEvent.getY() - size / 2;
+
+            gc.clearRect(x,y,size,size);
+        });
+    }
+
 
 
     void makeACircle() {
 
-        if (circleButton.isSelected()) {
             GraphicsContext gc = canvas.getGraphicsContext2D();
             canvas.setOnMousePressed(mouseEvent -> {
                 double size = sizeOfBrush.getValue();
-                double x = mouseEvent.getX() - size / 2;
-                double y = mouseEvent.getY() - size / 2;
                 Circle circle = new Circle(size, size, colorPicker.getValue());
-                gc.fillRect(x, y, size, size);
                 circle.draw(gc);
 
             });
-        }
 
     }
 
-    @FXML
-    void makeARectangle(ActionEvent event) {
-        if (rectangleButton.isSelected()) {
-            shapes.add(new Rectangle(sizeOfBrush.getValue(), sizeOfBrush.getValue(), colorPicker.getValue()));
-        }
+
+    void makeARectangle() {
+
+        shapes.add(new Rectangle(sizeOfBrush.getValue(), sizeOfBrush.getValue(), colorPicker.getValue()));
+
     }
 
-    @FXML
-    void makeATriangle(ActionEvent event) {
-        if (triangleButton.isSelected()) {
-            shapes.add(new Triangle(sizeOfBrush.getValue(), sizeOfBrush.getValue(), colorPicker.getValue()));
-        }
+
+    void makeATriangle() {
+        shapes.add(new Triangle(sizeOfBrush.getValue(), sizeOfBrush.getValue(), colorPicker.getValue()));
     }
 
 

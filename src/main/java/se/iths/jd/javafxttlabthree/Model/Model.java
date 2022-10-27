4847edfccd0887922ee86c4bf8ef229d1082c6a6
Johannes Danielsson <danielssonjohannes@gmail.com>
2022-10-27 14:37:12 +0200

    Adding some get methods and removing to be able to do a triangle in Model.java and adding, so you can be able to change size on Circle

--- src/main/java/se/iths/jd/javafxttlabthree/Model/Model.java
+++ src/main/java/se/iths/jd/javafxttlabthree/Model/Model.java
@@ -1,6 +1,8 @@
 package se.iths.jd.javafxttlabthree.Model;
 
+import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.paint.Color;
 import se.iths.jd.javafxttlabthree.shapes.Circle;
@@ -13,48 +15,51 @@ import java.util.List;
 public class Model {
 
     List<Shapes> shapes;
-    ObjectProperty<Color>color;
+    ObjectProperty<Color> color;
+    DoubleProperty size;
 
-    public Model(){
+    public Model() {
 
         shapes = new ArrayList<>();
-        color= new SimpleObjectProperty<>(Color.BLACK);
+        color = new SimpleObjectProperty<>(Color.BLACK);
+        size = new SimpleDoubleProperty(4.3);
     }
 
 
-    public void addShapes(double x, double y){
-        if (rectangleISSSelected(true)){
-            shapes.add(new Rectangle(x,y,color.getValue()));
+    public void addShapes(double x, double y) {
+        if (rectangleISSSelected(true)) {
+            shapes.add(new Rectangle(x, y, color.getValue()));
         }
-        if (circleISSelected(true)){
-            shapes.add(new Circle(x,y,color.getValue()));
+        if (circleISSelected(true)) {
+            shapes.add(new Circle(x, y, color.getValue(), size.getValue()));
         }
     }
 
-    public boolean rectangleISSSelected(boolean isSelected){
-        if (isSelected== true){
+    public boolean rectangleISSSelected(boolean isSelected) {
+        if (isSelected == true) {
             return true;
         }
         return false;
     }
-    public boolean triangleISSelected(boolean isSelected){
-        if (isSelected==true){
+
+    public boolean circleISSelected(boolean isSelected) {
+        if (isSelected == true) {
             return true;
         }
+
         return false;
-    }
-    public boolean circleISSelected(boolean isSelected){
-        if (isSelected==true){
-            return true;
-        }
-
-            return false;
 
     }
-    public ObjectProperty<Color> getcolor(){
+
+    public ObjectProperty<Color> getcolor() {
         return color;
     }
+
     public List<Shapes> getShapes() {
         return shapes;
+    }
+
+    public DoubleProperty sizeProperty() {
+        return size;
     }
 }

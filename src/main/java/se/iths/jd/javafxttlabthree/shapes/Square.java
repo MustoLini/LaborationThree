195cfc31f657f804a63e49fae4ac6cf430ecafbd
Johannes Danielsson <danielssonjohannes@gmail.com>
2022-10-30 14:10:14 +0100

    Refactoring the Square.java and Square.draw so you will be able to draw now

---
+++ src/main/java/se/iths/jd/javafxttlabthree/shapes/Square.java
@@ -0,0 +1,33 @@
+package se.iths.jd.javafxttlabthree.shapes;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import se.iths.jd.javafxttlabthree.shapes.shapesMainClass.Shapes;
+
+import java.awt.event.MouseEvent;
+
+public class Square extends Shapes {
+    double radius;
+
+    public Square(double x, double y, Color color, double radius) {
+        super(x, y, color);
+        this.radius = radius;
+    }
+
+
+    @Override
+    public void draw(GraphicsContext graphicsContext) {
+        graphicsContext.setFill(getColor());
+        graphicsContext.fillRect(getX() - (radius / 2) - 2.5, getY() - (radius / 2) - 2.5, radius + 5, radius + 5);
+        graphicsContext.setFill(getColor());
+        graphicsContext.fillRect(getX() - (radius / 2), getY() - (radius / 2) - 2.5, radius, radius);
+
+
+    }
+
+    @Override
+    public void isSelected(MouseEvent mouseEvent) {
+
+    }
+    // TODO: 10/18/2022 Implement Shapes From Inteface Shapes
+}

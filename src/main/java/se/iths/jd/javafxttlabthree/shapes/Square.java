271d411057d7d10e22e40a411d776261b72b4c3b
MustoLini <danielssonjohannes@gmail.com>
2022-11-07 11:17:48 +0100

    Refactoring but still bug with Square.java

--- src/main/java/se/iths/jd/javafxttlabthree/shapes/Square.java
+++ src/main/java/se/iths/jd/javafxttlabthree/shapes/Square.java
@@ -5,7 +5,7 @@ import javafx.scene.paint.Color;
 import se.iths.jd.javafxttlabthree.shapes.shapesMainClass.Shapes;
 
 public class Square extends Shapes {
-    double radius;
+    private double radius;
 
     public Square(double x, double y, Color color, double radius) {
         super(x, y, color);
@@ -19,19 +19,18 @@ public class Square extends Shapes {
         graphicsContext.fillRect(getX() - (radius / 2) - 2.5, getY() - (radius / 2) - 2.5, radius + 5, radius + 5);
         graphicsContext.setFill(getColor());
         graphicsContext.fillRect(getX() - (radius / 2), getY() - (radius / 2) , radius, radius);
-
+        System.out.println(this);
 
     }
 
     @Override
     public boolean isSelected(double x, double y) {
-        double xArea = x - (radius / 2);
-        double yArea = y - (radius / 2);
+        double westSide = x - (radius /2);
+        double eastSide = x + (radius /2);
+        double lowerEdge= y - (radius /2 );
+        double upperEdge= y+ (radius /2);
 
-        return x >= xArea &&
-                x <= xArea + ((radius )) &&
-                y >= yArea &&
-                y <= yArea + ((radius ));
+        return (westSide<= x&& x<= eastSide)&& (lowerEdge<= y && y<= upperEdge);
 
     }
 
